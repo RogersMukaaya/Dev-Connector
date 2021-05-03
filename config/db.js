e170d8cd45f9ef6223fbe948078802d29cc361f0
rogers mukaaya <mukaayarogers7@gmail.com>
2021-05-03 15:55:28 +0300

    First Commit

---
+++ config/db.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+const config = require('config');
+const db = config.get('mongoURL');
+
+// Connect to the DB
+const connectDB = async () => {
+    try {
+        await mongoose.connect(db, {
+            useNewUrlParser: true 
+        });
+        console.log('MongoDB Connected')
+    } catch(err) {
+        console.error(err.message);
+        // Exist process with failure
+        process.exit(1);
+    }
+}
+
+module.exports = connectDB;
+ 
