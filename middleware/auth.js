cb6c1492bdf6b07755c52329572cc58278c6cc98
rogers mukaaya <mukaayarogers7@gmail.com>
2021-05-04 17:43:45 +0300

    Add middleware

---
+++ middleware/auth.js
@@ -0,0 +1,29 @@
+const jwt = require('jsonwebtoken');
+const config = require('config');
+
+
+// A middleware function is a function that
+// has access to the request and response object/cycle
+// next is function hat we have to run in order to run
+// the next middleware
+
+module.exports = function(req, res, next) {
+    // Get token from header
+    const token = req.header('x-auth-token');
+
+    // Check if not token
+    if(!token) {
+        // 401 - not found
+        return res.status(401).json({ msg: 'No token, authorization denied' });
+    }
+
+    // Verify token
+    try {
+        const decoded = jwt.verify(token, config.get('jwtSecret'));
+
+        req.user = decoded.user;
+        next();
+    } catch(err) {
+        res.status(401).json({ msg: 'Token is not valid' });
+    }
+}
--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const gravatar = require('gravatar');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('config');
 const { check, validationResult } = require('express-validator/check');
 
 const User = require('../../models/User');
@@ -29,7 +32,7 @@ router.post('/', [
         let user = await User.findOne({ email });
 
         if(user) {
-            res.status(400).json({ errors: [{ msg: 'User already exists' }] });
+            return res.status(400).json({ errors: [{ msg: 'User already exists' }] });
         }
 
         // Get users gravatar
@@ -39,16 +42,36 @@ router.post('/', [
             d: 'mm'
         });
 
-        
+        user = new User({
+            name,
+            email,
+            avatar,
+            password
+        })
 
         // Encrypt password
+        const salt = await bcrypt.genSalt(10);
+
+        user.password = await bcrypt.hash(password, salt);
+
+        await user.save();
 
         // Return jsonwebtoken
+        const payload = {
+            user: {
+                id: user.id
+            }
+        }
 
-
-
-
-        res.send('Users route');
+        jwt.sign(
+            payload, 
+            config.get('jwtSecrete'),
+            { expiresIn: 360000 },
+            (err, token) => {
+                if(err) throw err;
+                res.json({ token });
+            }
+        );
 
     } catch(err) {
         console.error(err.message);
