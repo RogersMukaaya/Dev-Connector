676c5a0cd5ad2494ab811a4b1d6333de8f502ae0
rogers mukaaya <mukaayarogers7@gmail.com>
2021-05-05 14:39:41 +0300

    Authenticate user on login

--- routes/api/users.js
+++ routes/api/users.js
@@ -65,7 +65,7 @@ router.post('/', [
 
         jwt.sign(
             payload, 
-            config.get('jwtSecrete'),
+            config.get('jwtSecret'),
             { expiresIn: 360000 },
             (err, token) => {
                 if(err) throw err;
