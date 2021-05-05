4ec41d1dfdc2f563a83c8ea8f115696231aca81e
rogers mukaaya <mukaayarogers7@gmail.com>
2021-05-05 12:27:23 +0300

    Set up auth route

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,10 +1,19 @@
 const express = require('express');
 const router = express.Router();
+const auth = require('../../middleware/auth');
+
+const User = require('../../models/User');
 
 // @route   GET api/auth
 // @desc.   Test route
 // @access  Public
 
-router.get('/', (req, res) => res.send('Auth route'));
+router.get('/', auth, (req, res) => {
+    try {
+        const user = await User.findById(req.user.id).select('-password')
+    } catch(err) {
+
+    }
+});
 
 module.exports = router;
