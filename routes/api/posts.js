e2f2ae68641b6af66a96ac80f0ddc094b80b4bb0
rogers mukaaya <mukaayarogers7@gmail.com>
2021-05-18 12:51:27 +0300

    Get a post by ID route

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -55,5 +55,26 @@ router.get('/', auth, async (req, res) => {
     }
 });
 
+// @route   GET api/posts/:id
+// @desc.   Get post by ID
+// @access  Private
+
+router.get('/:id', auth, async (req, res) => {
+    try {
+        const post = await Post.findById(req.params.id);
+
+        if(!post) {
+            return res.status(404).json({ msg: 'Post not found' });
+        }
+
+        res.json(post);
+    } catch (error) {
+        console.error(error.message);
+        if(err.kind === 'ObjectId') {
+            return res.status(404).json({ msg: 'Post not found' });
+        }
+        res.status(500).send('Server error');
+    }
+});
 
 module.exports = router;
